4729c4aa624c9bea9ee6a29ef4560f2df6b36e4b
novalhabibi <34689831+novalhabibi@users.noreply.github.com>
2019-03-25 10:38:41 +0700

    Penambahan modal dll

--- application/views/admin/_includes/navbar.php
+++ application/views/admin/_includes/navbar.php
@@ -7,10 +7,19 @@
     </button>
 
     <!-- Navbar Search -->
-   
+    <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+        <div class="input-group-append">
+          <button class="btn btn-primary" type="button">
+            <i class="fas fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </form>
 
     <!-- Navbar -->
-    <ul class="navbar-nav ml-auto ml-md-0 pliud-right">
+    <ul class="navbar-nav ml-auto ml-md-0">
       <li class="nav-item dropdown no-arrow">
         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <i class="fas fa-user-circle fa-fw"></i>
